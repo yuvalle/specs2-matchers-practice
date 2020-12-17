18dfebe19fe6a647cece71a59bca1a003dff32ba
yuvalle <yuvall@wix.com>
2020-12-17 08:34:32 +0200

    Revert "First commit to add maven skeleton project."
    This reverts commit d8498b61a8a650bbff756c00ef66cc1b2c4ab060.

--- src/main/java/com/example/JavaMain.java
+++
@@ -1,7 +0,0 @@
-// package com.example;
-
-// public class JavaMain extends JavaLogging {
-//     public static void main(String[] args) {
-//         log.info("Hello World");
-//     }
-// }
--- src/main/scala/com/example/Main.scala
+++
@@ -1,6 +0,0 @@
-package com.example
-
-object Main extends App with Logging {
-  def msg = "Hello World"
-  log.info(msg)
-}
